a9e0024ac1390725219682457e2b20b6189d2bec
k9lvn <kevinchen828@hotmail.com>
2021-03-13 22:29:27 -0500

    ...

--- frontend/src/actions/photos.ts
+++ frontend/src/actions/photos.ts
@@ -1,12 +1,11 @@
 import axios from "axios";
-import { BASE_API_URL } from "../utils/constants";
 // import { getErrors } from './errors';
 
 export const beginAddPhoto = async (photo: any) => {
   try {
     const formData = new FormData();
     formData.append("photo", photo);
-    const res = await axios.post<string>(`${BASE_API_URL}/photos`, formData, {
+    const res = await axios.post<string>("/photos", formData, {
       headers: {
         "Content-Type": "multipart/form-data",
       },
@@ -19,7 +18,7 @@ export const beginAddPhoto = async (photo: any) => {
 
 export const startLoadPhotos = async () => {
   try {
-    const photos = await axios.get<string[]>(`${BASE_API_URL}/photos`);
+    const photos = await axios.get<string[]>("/photos");
     console.log("photos ", photos);
     return photos;
   } catch (error) {
--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,15 +1,24 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+let https = require("https");
+
+// module.exports = (app) => {
+//   app.use(
+//     "/photos",
+//     createProxyMiddleware({
+//       target: "https://k9lvn-photo-gallery-app.herokuapp.com/photos",
+//       changeOrigin: true,
+//       agent: https.globalAgent,
+//       secure: false,
+//     })
+//   );
+// };
 
 module.exports = (app) => {
   app.use(
-    "/photos",
-    createProxyMiddleware({
-      target: "https://k9lvn-photo-gallery-app.herokuapp.com/photos",
+    createProxyMiddleware("/photos", {
+      target: "http://localhost:3300/photos",
       changeOrigin: true,
-      secure: false
-      // pathRewrite: {
-      //   "^/api": "/api/v1"
-      // }
     })
   );
 };
