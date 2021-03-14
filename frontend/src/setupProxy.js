0b6d38c49a7c03edcb47dcbee48907e71ee2190e
k9lvn <kevinchen828@hotmail.com>
2021-03-13 22:35:36 -0500

    fixed mistake

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,23 +2,14 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 let https = require("https");
 
-// module.exports = (app) => {
-//   app.use(
-//     "/photos",
-//     createProxyMiddleware({
-//       target: "https://k9lvn-photo-gallery-app.herokuapp.com/photos",
-//       changeOrigin: true,
-//       agent: https.globalAgent,
-//       secure: false,
-//     })
-//   );
-// };
-
 module.exports = (app) => {
   app.use(
-    createProxyMiddleware("/photos", {
-      target: "http://localhost:3300/photos",
+    "/photos",
+    createProxyMiddleware({
+      target: "https://k9lvn-photo-gallery-app.herokuapp.com",
       changeOrigin: true,
+      // agent: https.globalAgent,
+      // secure: false,
     })
   );
 };
