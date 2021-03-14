c6737ba46b7b61168f1b2bbbe85c50d5600a1efe
k9lvn <kevinchen828@hotmail.com>
2021-03-13 22:53:38 -0500

    allow cors

--- index.ts
+++ index.ts
@@ -6,6 +6,13 @@ import "./db/connection";
 const app = express();
 const PORT = process.env.PORT || 3300;
 
+let corsOptions = {
+  origin: "https://kind-sinoussi-6afd22.netlify.app",
+  optionsSuccessStatus: 200, // For legacy browser support
+};
+
+app.use(cors(corsOptions));
+
 // app.use(cors()); // allows restricted resources on a web page to be requested from another domain outside the domain from which the first resource was served
 app.use(photosRouter);
 
